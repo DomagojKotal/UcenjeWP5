b0020b5346e34a662dc95abbcc902f89a7ecaed9
lodi432 <glavacevic.d@gmail.com>
2024-05-25 13:02:54 +0200

    Update fakultet.sql

--- fakultet.sql
+++ fakultet.sql
@@ -1,1 +1,17 @@
---create database fakultet
+----create database fakultet
+
+----use fakultet;
+--create table IspitniRok(
+--sifra int primary key
+--predmet varchar (50)
+--vrsta ispita varchar (20)
+--datum datetime 
+--pristupio int
+--);
+
+--create table pristupnici(
+--ispitnirok datetime
+--student varchar(20)
+--broj bodova int
+--ocijena int
+--);
