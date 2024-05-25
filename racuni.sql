fcd20b6d134e79f588243b3a4d98408f2749999f
DomagojKotal <kotal.domagoj1995@gmail.com>
2024-05-25 15:53:49 +0200

    Create racuni.sql

---
+++ racuni.sql
@@ -0,0 +1,16 @@
+--create database racuni;
+--use racuni;
+--create table racun(
+--sifra int primary key;
+--rednibroj int;
+--datum datetime
+--kupac varchar (20);
+--placeno int
+--);
+--create table stavkeracuna(
+--racun int
+--artikl int
+--cijena int
+--kolicina int
+--popust int
+--)
